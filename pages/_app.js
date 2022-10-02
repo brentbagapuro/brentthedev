7243089e35773866908a8d1d2ca61b9897283932
brentbagapuro <brentbagapuro@gmail.com>
2022-10-02 15:44:18 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Microservice 2</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
